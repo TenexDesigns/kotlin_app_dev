9dd97b61a9c78720f98585bfc103ba1913138c97
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-02-10 20:34:33 +0300

    Create BroadcastReceivers.kt

---
+++ BroadcastReceivers.kt
@@ -0,0 +1,23 @@
+Broad cast recivers
+
+Android apps can send or receive broad cast messages from the android system  and other apps.
+These broadcasters are sent when an event  of intersrest occurs .
+e.g When the system boots up,When the device starts 
+Apps can also snd custom broadcasts e.g to notify other apps of something they may be intrested in .e.g data that has been downloaded
+
+
+
+Receiving Broad casts
+
+There are two ways of receiving broadcasrts in our app
+
+
+Context-registerd recivers
+manifest-declared recivers
+
+Sending broadcast 
+   - We use the  sendBroadcast(intent) method
+
+
+
+Context-regesterd receivers
