b1e8b0edf00231b8d79dc2a7095bde9743040b91
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-02-15 13:59:15 +0300

    Create Styles & Themes.kt

---
+++ Styles & Themes.kt
@@ -0,0 +1,18 @@
+Style
+
+A style is a collection of attributes that specify the appearance for a VIEW,e.g button, or WINDOW
+A style can specify attributes such as font,color,font Size and background color
+         e.g
+     <style name="CustomFontStyle">
+      <item name="android:layout_width">fill_parent</item>
+      <item name="android:layout_height">wrap_content</item>
+      <item name="android:capitalize">characters</item>
+      <item name="android:typeface">monospace</item>
+      <item name="android:textSize">12pt</item>
+      <item name="android:textColor">#00FF00</item>/> 
+   </style>
+   
+   
+   Theme ,
+   A theme is a collection of attributes that applied to an entire APP,ACTIVITY  or VIEW HIERARCHY (not just an individual view)
+   Themes can also apply styles to NON-VIEW ELEMENTS  such as ,the status bar, and window background. 
