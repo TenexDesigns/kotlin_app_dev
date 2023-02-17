5637a756d61f93f8e71b7e32bceff7de428da9ad
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-02-17 13:28:10 +0300

    Create Content_Provider.kt

---
+++ Content_Provider.kt
@@ -0,0 +1,22 @@
+Content Providers are used to share data of one application with other application in Android. 
+For example, the details of contacts stored in your mobile device with the help
+of Contacts application is shared with other applications like WhatsApp, Facebook
+
+
+
+
+
+  
+
+                   your-content   <-----------> other-Applications
+                   provider
+                      ^
+                      |   
+                      |  
+                      |
+                      V
+                   your app data
+
+
+
+
