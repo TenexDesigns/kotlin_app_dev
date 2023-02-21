ebbe3897196f98e3fa1a542cca8764c30bc0cd8a
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-02-21 16:09:49 +0300

    Create Android_ui.kt

---
+++ Android_ui.kt
@@ -0,0 +1,45 @@
+THE MAJOR  UI COMPONENTS PRESENT IN ANY ANDROID APPLICATION
+They are 
+
+----> Main Action bar
+----> View control
+----> Content area
+----> Split Action Bar
+
+
+View
+A view is considered as a basic building block for a proper user interface that is created from the view class 
+It is a box that responds to user interaction ,eg button,check box e.tc
+It is something that the user can see and interact with.
+
+View Group
+Is an invisible container of other views or even view groups
+Invisible container that defines the layout structure for view and other view group objects 
+                                                   
+                                   ViewGroup                
+                                      |             
+                        ______________|_____________                          
+                       |              |             |                             
+                   viewGroup        view          view                                
+                       |                      
+               ________|_________
+              |        |         |
+            view      view      view                                     
+                                                   
+                                                   
+ Layouts
+
+
+A layout defines the structure of the UI of your application
+All elements in a layout are built usng a hierachy of view and view group objects
+
+                                                   
+                                                   
+                                                   
+
+
+
+
+
+
+
