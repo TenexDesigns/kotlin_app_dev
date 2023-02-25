93af57670541c40f4540397e6cfc6bdea6557df1
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-02-25 11:37:29 +0300

    Update Toolbar.kt

--- Toolbar.kt
+++ Toolbar.kt
@@ -26,3 +26,21 @@ The tool bar can have
 - Overflow menu
 - Naviagtion button
 
+
+
+
+
+
+SNACKBAR
+
+It is a lightweight feedback like toast.  e.g when u delete a message ,you get a tast like message with an UNDO option to undo the done opertion
+It disappers afert a timeout  --> setTimeout
+Can be swipped off the screen  --> Usig coordinator layout
+may contain an action or not
+
+
+Types of snackbar
+simple snackbar --No action
+Custom Color snack bar ---Can be customised in terms of color e.tc
+Snackbar with option -- > Give user an option e.g to undo the action.
+
