348f05f440495f78d962d0136cab0791ce9d0fa9
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-02-25 10:40:14 +0300

    Create Toolbar.kt

---
+++ Toolbar.kt
@@ -0,0 +1,28 @@
+Tool bar 
+
+
+Used to give app identity
+Indicates users location in app
+Used to acess important actions
+Support for easy view switching
+You can show multiple tool bar on screen
+
+Compatible for API 7+
+Simialr to action bar
+To use ActionBar as toool bar
+  . setSupportActionBar
+  . setActionBar
+
+Tool bar is a view group - Meaning it has its own layout and can be placed anywhere on the screen.
+                         - It can have multiple views as child view
+                         - It can contain custom views
+
+
+
+The tool bar can have
+-Title
+-Subtitle
+-Menu Icon
+- Overflow menu
+- Naviagtion button
+
