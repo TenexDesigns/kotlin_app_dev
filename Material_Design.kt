595e266ed66218c8df2aae40ade8d071ac8f009e
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-02-28 07:50:51 +0300

    Create Material_Design.kt

---
+++ Material_Design.kt
@@ -0,0 +1,28 @@
+Density Buckets
+
+Google assignes differnt density buckets to devices with diffrent density screens.
+A device with 442 density screen might be assigned to a 480dpi density bucket.
+But genreally you only have to work with the middle four density buckets. I,E 1X ,1.5X ,2X, 3 X 
+These buckets give you the control of the scallling of your graphic assets.i.e things like icons
+Thus you dont have to make diffrent if not hundreds versions of icons for diffrent screen density ,you can just scale the icons based on its density buckets
+
+DENSITY BUCKETS
+
+
+LDPI           120dpi     .75x
+MDPI           160dpi     1x
+HDPI           240dpi     1.5x
+XHDPI          320dpi     2x
+XXHDPI         480dpi     3x
+XXXHDPI        640dpi     4x
+
+
+
+
+
+
+
+ASSET SHARPNESS / CRISPNESS
+Nor mally it is good to provide assets for the for major density buckets .
+But instaetd ,you can provide assets for the XXHDPI  or XHDPI and let android scale them down.
+Now android is not suited for down scalling but you can create icons for these density buckets from the ANDROID ASSETS STUDDIO// https://romannurik.github.io/AndroidAssetStudio/ 
