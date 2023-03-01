b8524ed0d128781d1c3817992e4e994f1fe6e4e8
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-03-01 12:01:28 +0300

    Update Toolbar.kt

--- Toolbar.kt
+++ Toolbar.kt
@@ -26,6 +26,41 @@ The tool bar can have
 - Overflow menu
 - Naviagtion button
 
+WHEN WE USE THE TOOL BAE ,WE USE IT A VIEW GROUP
+
+To get started with the tool bar we have to set it in the activity we want it to appear
+We have to put it in thx xml of the activty we want the toool bar to appear.
+We instanciate the tool bar by using the foolowing tag   <androidx.appcompat.widget.Toolbar>
+
+  
+  // THIS IS THE XML FILE OF THE ACTIVITY WE WANT OUR APP BAR TO APPEAR
+<?xml version="1.0" encoding="utf-8"?>
+<androidx.constraintlayout.widget.ConstraintLayout xmlns:android="http://schemas.android.com/apk/res/android"
+    xmlns:app="http://schemas.android.com/apk/res-auto"
+    xmlns:tools="http://schemas.android.com/tools"
+    android:layout_width="match_parent"
+    android:layout_height="match_parent">
+  
+  // THIS IS OUR APP BAR
+
+    <androidx.appcompat.widget.Toolbar
+        android:id="@+id/toolbar2"
+        android:layout_width="0dp"
+        android:layout_height="?attr/actionBarSize"
+        android:background="@color/teal_700"
+        app:layout_constraintEnd_toEndOf="parent"
+        app:layout_constraintStart_toStartOf="parent"
+        app:layout_constraintTop_toTopOf="parent"
+        tools:ignore="MissingConstraints">
+
+    </androidx.appcompat.widget.Toolbar>
+
+</androidx.constraintlayout.widget.ConstraintLayout>
+
+We can define the toolbar as a layoutfile of the menu type and in it we define , what we want to be dispalyed
+In the tool bar we can disp
+
+
 
 
 
