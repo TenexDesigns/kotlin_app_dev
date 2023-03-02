511ae366a8b53a115f919e098515e2d56d2d718d
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-03-02 22:16:04 +0300

    Update Activity_Transition.kt

--- Activity_Transition.kt
+++ Activity_Transition.kt
@@ -58,6 +58,33 @@ The shared elemts will be used to move from that activit to this one
 To enable that mwe have to use the same transtion name s for this shared view
 
 
+Now in the secondActity.kt file we have to implememnt the
+ getWindow().requestFeature()Window.FEATURE_CONTENT_TRASITIONS) method before the onCreate method To enable transtions to happenn
+OR WE CAN DO THIS IN THE THEME FILE
+----> <item name="android:windowContentTransitions">true<item/>
+ //This enables transitoons to occcur throughout the app.
+ 
+
+
+<style name="Theme.App" parent="Theme.AppComaapct.light">
+
+----> <item name="android:windowContentTransitions">true<item/>
+ 
+ <style/>
+
+ 
+ __> Now wehave to implement the ActivityOptions.makeSceneTrabstion() method to enable trastions to happen
+This method takes in the context and the view to be shared. If the views are many then we have to put them in an array of key,value pair
+val arrayPair = arrayof(image,"sharedImage")
+val arrayPair = arrayof(image,"sharedText")
+
+val options:ActivityOptions = ActivityOptions.makeSceneTrabstion(this,arrayPair) ;;We put the items to be shared in an array and then pass that array as the second arumntet here to form the option s varialble
+
+intent = Intent(MainActivity.this,secondActivity::class.java) // This tells android to move from the mainactivity to the secondActivity
+startActivity(intent,options.bundle())  // This is what implements the intent to move to the second aciviyt. We combine the intent with the shared eeeeelemnts  in the options array.
+
+
+
 
 
 
