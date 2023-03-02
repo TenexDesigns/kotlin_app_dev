27eca7ef0f5fed2bcea8fb5c4f645d8052e4e6f7
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-03-02 15:24:54 +0300

    Create Snackbar.kt

---
+++ Snackbar.kt
@@ -0,0 +1,27 @@
+Snack Bar
+
+
+It is a light weight feedback like toast
+It dissaoears affter a timeout --> Time out is set using the method --> Set duration
+The snack bar cn be swiped off the screen when used together with the co-ordiator lyout 
+The snack bar may contain an action.
+
+Types of snckbars we can use inour app
+
+----> Simple snack bar
+Dispalys a simple message 
+Contains no actions 
+
+----> Custom Color Snack bar
+Dispalys a simple message 
+Contains no actions 
+Has a custom color e.g red ,or green of any color you chooose
+
+
+-----> Snack bar with option or action
+
+
+
+
+ 
+
