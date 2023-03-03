e2e99b39b95a7e8c5dcb9c378b61d93e036c77db
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-03-03 12:25:55 +0300

    Create Recycler_view.kt

---
+++ Recycler_view.kt
@@ -0,0 +1,14 @@
+RECYCLER VIEW 
+
+Recycler view simplifies display and handling of large data sets since it can be scrolled.
+
+
+It has layoutmanagers for positioning items
+It has default animations for commmon operations
+
+
+Some Built layout managers
+
+-> LinearLayout MAnger  ->Items are arrangerd verticlay
+-> GridLayoutmanager    -> Itemes are arranged in a grid with colums and rows that have same size
+-> StaggeredGridLayoutManager -> Items in a grid with items with different sizes
