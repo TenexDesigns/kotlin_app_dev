4189b1f9085d2ec7320fd1172a387a5ff58dd9f0
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-03-03 12:20:36 +0300

    Create Navigation-View.kt

---
+++ Navigation-View.kt
@@ -0,0 +1,10 @@
+This is used together with the drawer icon to dislay items in the drawer menu
+
+
+The items can be displeyed as
+
+items only
+ittems in group without header
+Items in group  with header.
+
+Do more reserarch on this.
