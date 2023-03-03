4f02d3251df152c45bc3c4d33bee0a94d4781c51
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-03-03 13:44:20 +0300

    Update Recycler_view.kt

--- Recycler_view.kt
+++ Recycler_view.kt
@@ -12,3 +12,80 @@ Some Built layout managers
 -> LinearLayout MAnger  ->Items are arrangerd verticlay
 -> GridLayoutmanager    -> Itemes are arranged in a grid with colums and rows that have same size
 -> StaggeredGridLayoutManager -> Items in a grid with items with different sizes
+
+
+
+
+CARD VIEW
+
+Shows information in cards
+
+It extends the frameLayout classs
+It gives consistent look across the platform
+The card view can have shadows and rounded corners
+
+
+RECYCLER WITH CARD VIEW
+
+--> Cear list item layout using cardView widgert
+--> Add recyclerView to activity_main.xml
+--> Provide data model to feed recycler view
+--> Custom adapter for recycler view - to provide view to represent items in data set
+--> Link custom adapter to recycler view
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
