edaa661d1dbd97cddfd525283cffd3c48689a3a4
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-03-04 10:44:36 +0300

    Update AlertDialog.kt

--- AlertDialog.kt
+++ AlertDialog.kt
@@ -25,6 +25,105 @@ The alert can be created using the AlertDialog.Builder class
                    .show()  //We put the dialog box here for the dialog box to be called
         }
 
+        
+        THERE ARE MATERIAL DESIGN DIALOGS
+
+
+Dialogs inform users about a task and can contain critical information, require decisions, or involve multiple tasks.
+
+IMPLEMENTATION OF DIALOG BUTTONS
+
+MaterialAlertDialogBuilder(context)
+    // Add customization options here
+    .show()
+    
+    
+    TYPES OF DIALOGS
+
+1. Alert dialog, 
+2. Simple dialog,
+3. Confirmation dialog,
+4. Full-screen dialog
+
+
+A dialog is a type of modal window that appears in front of app content to provide critical information or ask for a decision.
+
+Dialogs disable all app functionality when they appear, and remain on screen until confirmed, dismissed, or a required action has been taken.
+
+Dialogs are purposefully interruptive, so they should be used sparingly.
+
+
+
+
+ALERT DIALOGS
+
+Alert dialogs interrupt users with urgent information, details, or actions.
+
+
+IMPLEMENTATION
+
+MaterialAlertDialogBuilder(context)
+        .setTitle(resources.getString(R.string.title))
+        .setMessage(resources.getString(R.string.supporting_text))
+        .setNeutralButton(resources.getString(R.string.cancel)) { dialog, which ->
+            // Respond to neutral button press
+        }
+        .setNegativeButton(resources.getString(R.string.decline)) { dialog, which ->
+            // Respond to negative button press
+        }
+        .setPositiveButton(resources.getString(R.string.accept)) { dialog, which ->
+            // Respond to positive button press
+        }
+        .show()
+
+        
+        SIMPLE DIALOG
+
+Simple dialogs can display items that are immediately actionable when selected. 
+They don’t have text buttons.
+
+As simple dialogs are interruptive, they should be used sparingly.
+Alternatively, dropdown menus provide options in a non-modal, less disruptive way.
+
+EXAMPLE OF IMPLIMENTATION
+
+val items = arrayOf("Item 1", "Item 2", "Item 3")
+
+MaterialAlertDialogBuilder(context)
+        .setTitle(resources.getString(R.string.title))
+        .setItems(items) { dialog, which ->
+             // Respond to item chosen
+         }
+        .show()
+        
+        
+        
+        
+        CONFIRMATION DIALOG
+
+Confirmation dialogs give users the ability to provide final confirmation of a choice 
+before committing to it, so they have a chance to change their minds if necessary.
+
+
+
+val singleItems = arrayOf("Item 1", "Item 2", "Item 3")
+val checkedItem = 1
+
+MaterialAlertDialogBuilder(context)
+        .setTitle(resources.getString(R.string.title))
+        .setNeutralButton(resources.getString(R.string.cancel)) { dialog, which ->
+            // Respond to neutral button press
+        }
+        .setPositiveButton(resources.getString(R.string.ok)) { dialog, which ->
+            // Respond to positive button press
+        }
+        // Single-choice items (initialized with checked item)
+        .setSingleChoiceItems(singleItems, checkedItem) { dialog, which ->
+             // Respond to item chosen
+         }
+        .show()
+
+
 
 
 
